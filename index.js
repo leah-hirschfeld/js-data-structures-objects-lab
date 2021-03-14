8aa4667efb6db99152946c7e507c45801aafb744
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-03-14 10:03:37 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const driver = {
+    name: 'Leah',
+    streetAddress: '123 Magic Rd'  };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newObj = { ...driver };
+
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    const newObj = Object.assign(driver,{[key]: value});
+
+    return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    
+    delete driver[key];
+
+    return driver;
+}   
+
+function deleteFromDriverByKey(driver, key) {
+    const newObj = { ...driver };
+
+    delete newObj[key]
+
+    return newObj
+}   
